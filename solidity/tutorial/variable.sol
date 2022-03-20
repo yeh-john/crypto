0b45f136f7b1b22c8f87f640bb7f25d0dbe6ab3d
John <88310061+yeh-john@users.noreply.github.com>
2022-03-20 10:22:21 -0400

    Create variable.sol

---
+++ solidity/tutorial/variable.sol
@@ -0,0 +1,14 @@
+pragma solidity 0.8.13; /// You need enter the version
+
+/// Add class
+contract test{
+    int a = -5; /// Like python int
+    uint b = 3; /// Only positive number
+    bool c = true; /// Only true and false
+    string d = "hello world"; /// Like python str
+    bytes10 e = "Only10Word"; /// Only can type 10 word 
+    bytes32 f = "Only32Word"; /// bytes have a lot of types
+    bytes3 g = "ETH"; /// bytes a lot of time use like this
+    address h = "0x95ad61b0a150d79219dcf64e1e6cc01f0b64kjke"; /// address
+    /// Solidity dosen't have float type. Because a lot of time like this 0.000134eth
+}
